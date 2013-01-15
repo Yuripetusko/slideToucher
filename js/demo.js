817daa180eaaa0481a8bf474089d5985ba7e6386
unknown <ypetusko@clymene.salmon.ltd.uk>
2013-01-15 16:40:45 +0000

    Adding numbers to slide

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,11 @@
 $("document").ready(function(){
 	var hue = 0;
-	Array.prototype.slice.call(document.querySelectorAll('.slide')).forEach(function(mc) {
-	    mc.style.backgroundColor = 'hsla(' + hue + ', 75%, 50%, 0.5)';
+	Array.prototype.slice.call(document.querySelectorAll('.slide')).forEach(function(element, i) {
+		var numberNode = document.createTextNode(i+1);
+		var number = document.createElement("span");
+		number.appendChild(numberNode);
+		element.appendChild(number);
+	    element.style.backgroundColor = 'hsla(' + hue + ', 75%, 50%, 0.5)';
 	    hue += 222.5;
 	});
 	
