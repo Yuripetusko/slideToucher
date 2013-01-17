94cdf98067be4a62257c70c28eda1ab93f4b4116
Yuri <yuripetusko@gmail.com>
2013-01-17 14:35:29 +0000

    fixed credit

--- js/touchSlide.js
+++ js/touchSlide.js
@@ -11,6 +11,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+Plugin is loosely based on tutorial by Martin Kool https://twitter.com/mrtnkl
+http://mobile.smashingmagazine.com/2012/06/21/play-with-hardware-accelerated-css/
+
 ************/
 
 // the semi-colon before function invocation is a safety net against concatenated
