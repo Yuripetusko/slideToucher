99ba7f4d060ae809bbfb916c4955d88728eb4841
Yuri <yuripetusko@gmail.com>
2013-01-17 13:55:35 +0000

    removing debug msg for now

--- js/touchSlide.js
+++ js/touchSlide.js
@@ -72,7 +72,7 @@
             var transition_time = 1 / (distance_delta / time_delta);
             //Let's make transition speed based on screen size, giving 1024x768 screen a max 1ms speed 
             var screenSizeDelta = dir === "Y" ? slideHeight / 768 : slideWidth / 1024;
-            $("#debug").html(Math.min(transition_time * screenSizeDelta, screenSizeDelta) / 3);
+            //$("#debug").html(Math.min(transition_time * screenSizeDelta, screenSizeDelta) / 3);
             return Math.min(transition_time * screenSizeDelta, screenSizeDelta) / 3;
         };
 
