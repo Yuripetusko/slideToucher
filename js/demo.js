9c2b49cf99be776372e2c3047ab95cd8369f7537
Yuri <yuripetusko@gmail.com>
2013-01-18 10:43:17 +0000

    Adding arrows indicating that vertical and horizontal swipe is possible

--- js/demo.js
+++ js/demo.js
@@ -8,6 +8,12 @@ $("document").ready(function(){
 	    element.style.backgroundColor = 'hsla(' + hue + ', 75%, 50%, 0.5)';
 	    hue += 222.5;
 	});
+
+	$("body").prepend("<span class=\"arrow left\"></span><span class=\"arrow top\"></span><span class=\"arrow right\"></span><span class=\"arrow bottom\"></span>");
+
+	setTimeout(function(){
+		$(".arrow").fadeOut(900);
+	}, 3000);
 	
 	$("#slides").touchSlide({
 		vertical: true,
